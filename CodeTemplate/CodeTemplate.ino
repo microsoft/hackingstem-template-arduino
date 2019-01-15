322f599e1acf6a81e6ac685d1da104eb2440c3b2
Adi Azulay <adazulay@microsoft.com>
2019-01-15 09:37:46 -0800

    Initial Commit, still needs code review + testing

--- CodeTemplate/CodeTemplate.ino
+++ CodeTemplate/CodeTemplate.ino
@@ -74,7 +74,6 @@ void loop()
   }
 }
 
-
 // SENSOR INPUT CODE-----------------------------------------------------------
 void processSensors() 
 {
@@ -132,21 +131,22 @@ void processIncomingSerial()
 // Gathers bytes from serial port to build inputString
 char* GetSerialData()
 {
-  static char inputString[64];
-  memset(inputString, 0, sizeof(inputString));
+  static char inputString[64]; // Create a char array to store incoming data
+  memset(inputString, 0, sizeof(inputString)); // Clear the memory from a pervious reading
   while (Serial.available()){
-    Serial.readBytesUntil('\n', inputString, 64);
+    Serial.readBytesUntil('\n', inputString, 64); //Read every byte in Serial buffer until line end or 64 bytes
   }
   return inputString;
 }
 
+// Seperate the data at each delimeter
 void parseData(char data[])
 {
-    char *token = strtok(data, ",");
-    int index = 0;
-    while (token != NULL){
-      arr[index] = token;
-      token = strtok(NULL, ",");
-      index++;
+    char *token = strtok(data, ","); // Find the first delimeter and return the token before it
+    int index = 0; // Index to track storage in the array
+    while (token != NULL){ // Char* strings terminate w/ a Null character. We'll keep running the command until we hit it
+      arr[index] = token; // Assign the token to an array
+      token = strtok(NULL, ","); // Conintue to the next delimeter
+      index++; // incremenet index to store next value
     }
 }
